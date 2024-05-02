561e56c9f0510f4f5a1008b8701eb1c99ca56c8b
sridhar <149560572+sridharan1810@users.noreply.github.com>
2024-05-02 18:25:36 +0530

    env updated

--- base/src/common/services/email.service.ts
+++ base/src/common/services/email.service.ts
@@ -73,7 +73,7 @@ export class EmailService {
         SendGrid.setApiKey(process.env.SENDGRID_KEY);
         const message: SendGrid.MailDataRequired = {
             to: verification.user.email,
-            from: process.env.ZAUTO_EMAIL,
+            from: process.env.CONVERTO_EMAIL,
             subject: (verification.type == VerificationType.VERIFYEMAIL) ? 'Welcome to ZautoAI' : 'Reset Account Password',
             html: template
         };
@@ -90,7 +90,7 @@ export class EmailService {
             .replaceAll('{{createdAt}}', signupMailDto.user.createdAt.toDateString())
         const message: SendGrid.MailDataRequired = {
             to: process.env.SIGNUP_ALERT_MAIL,
-            from: process.env.ZAUTO_EMAIL,
+            from: process.env.CONVERTO_EMAIL,
             subject: 'Signup Aleart',
             html: template
         };
