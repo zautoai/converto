3afda625cdb546d3c9a07afc45a7aa3f8c96ad0c
sridhar <149560572+sridharan1810@users.noreply.github.com>
2024-05-03 12:14:59 +0530

    base app api url changed

--- base/client/src/environments/environment.dev.ts
+++ base/client/src/environments/environment.dev.ts
@@ -3,5 +3,5 @@ export const environment = {
   
     apiRoot: 'https://beta.converto.biz/',
     baseUrl: 'https://beta.converto.biz/',
-    zauto_bot_url:'https://bot.zautoai.com/'
+    zauto_bot_url:'https://bot.zautoai.com/' 
 };
