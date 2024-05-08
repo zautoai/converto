43ec998bdd4dfe2c706bd4cb471d81571cde192b
sridhar <149560572+sridharan1810@users.noreply.github.com>
2024-05-08 12:39:41 +0530

    external crm contact mapping completed

--- base/client/src/app/pages/plugins/crm-mapping/crm-mapping.component.ts
+++ base/client/src/app/pages/plugins/crm-mapping/crm-mapping.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { API } from 'src/app/config/endpoint.config';
 import { RestService } from 'src/app/shared/services/rest.service';
 
@@ -9,9 +9,15 @@ import { RestService } from 'src/app/shared/services/rest.service';
 })
 export class CrmMappingComponent implements OnInit{
 
+  isLoading:boolean = false;
   @Input()crmName:string | undefined;
   contactFields: string[] = [];
   externalCrmFields: any[] = [];
+
+  selectedOptions: { [key: string]: string } = {};
+
+  @Output() onSubmit = new EventEmitter<any>();
+  @Output() onCancel = new EventEmitter<any>();
 
   constructor(
     private readonly restService: RestService
@@ -20,6 +26,7 @@ export class CrmMappingComponent implements OnInit{
   ngOnInit(): void {
     this.getContactFields();
     this.getExternalCrmField();
+    this.getMappingData();
   }
 
   getContactFields()
@@ -36,18 +43,56 @@ export class CrmMappingComponent implements OnInit{
   getExternalCrmField()
   {
     if(!this.crmName) return;    
+    this.isLoading = true;
     this.restService.getAll(API.main.external_crm + `/fields/contacts/${this.crmName}`).subscribe((res:any) => {
       this.externalCrmFields = [...res];      
+      this.isLoading = false;
     },
     err => {
       console.log(err);
+      this.isLoading = false;
     });
   }
 
   getMappingData() {
-    this.restService.getAll('crm-mapping').subscribe(res => {
-      console.log(res);
+    this.restService.getAll(API.main.external_crm + `/mappings/${this.crmName as string}`).subscribe((res:any) => {
+      this.handleMapping( res || []);
     });
   }
 
+  private handleMapping(data:any[])
+  {
+    for (const item of data) {
+      this.selectedOptions[item.fieldName] = item.externalCRMFieldName;
+    }
+  }
+
+  cancel() {
+    this.onCancel.emit();
+  }
+
+  submit() {
+    if(!this.crmName) return;
+    const fields = Object.keys(this.selectedOptions);
+    if(fields.length === 0) return;
+    const mappings = [];
+    for (const field of fields) {
+      mappings.push({
+        objectType:'Contact',
+        fieldName: field,
+        externalCRMObjectType: 'Contact',
+        externalCRMFieldName: this.selectedOptions[field],
+        crmName: this.crmName
+      });
+    }   
+
+    this.restService.post(API.main.external_crm + `/mappings`, {mappings}).subscribe(res => {
+        console.log(res);
+        this.onSubmit.emit();
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
 }
