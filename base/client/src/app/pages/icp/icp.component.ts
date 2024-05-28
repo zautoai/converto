7fae1c613a1d8c1b2b1a5cf386a0e9cabcaa09d8
sakthi vignesh <sakthi@zautoai.com>
2024-05-28 15:49:22 +0530

    icp view added

--- base/client/src/app/pages/icp/icp.component.ts
+++ base/client/src/app/pages/icp/icp.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { NgbModal, NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 import { API } from 'src/app/config/endpoint.config';
 import { NotificationService } from 'src/app/shared/services/notification.service';
 import { RestService } from 'src/app/shared/services/rest.service';
@@ -12,14 +14,22 @@ import { SweetAlertService } from 'src/app/shared/services/sweet-alart.service';
 })
 export class IcpComponent {
 
-  icpdata: any = []
+  @ViewChild('updateUserOffcanvas') updateUserOffcanvas: ElementRef | undefined;
 
+
+errorFeedback: any = { title: '', describe: '' };
+  icpdata: any = []
+  user: any = {};
+  
+  isEdit: boolean = false;
 
   constructor(
     private restService: RestService,
     private notifService: NotificationService,
+    private modalService: NgbModal,
     private sweetAlertService: SweetAlertService,
-    private router: Router
+    private router: Router,
+    private offcanvasService: NgbOffcanvas,
   ) {
 
   }
@@ -32,6 +42,7 @@ export class IcpComponent {
     this.restService.getAll(API.main.icp).subscribe({
       next: (response: any) => {
         this.icpdata = response.data;
+        console.log("test", this.icpdata)
       },
       error: (error) => {
         console.error(error);
@@ -42,4 +53,47 @@ export class IcpComponent {
   handleCreate(): void {
     this.router.navigate(['/icp/create']);
   }
+  
+  
+  /**resterror */
+
+
+
+  delete = (data: any) => {
+    this.user = data;
+
+    this.sweetAlertService.warning(
+      'Delete user',
+      'Are you sure you want to delete ?',
+      ['Delete', 'Cancel'],
+      (confirm: any) => {
+        if (confirm.isConfirmed) {
+          this.confirmDelete(data);
+        }
+      },
+    );
+  };
+
+  confirmDelete = (data: any) => {
+    this.restService.delete(API.main.contact, data.id).subscribe(
+      (response: any) => {
+        this.notifService.showSuccess('Accounts Deleted Successfully.');
+        this.closeModal();
+        console.log(this.user);
+        this.getIcps()
+      },
+      (error) => {
+        console.error(error);
+        this.notifService.showError(error.error.message);
+      },
+    );
+  };
+  closeModal = () => {
+    this.user = {};
+    this.isEdit = false;
+    this.modalService.dismissAll();
+  };
+
 }
+
+
