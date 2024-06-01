dfedeefa53bfe601c6995ede9b28408800dfcbc7
sridhar <149560572+sridharan1810@users.noreply.github.com>
2024-06-01 23:29:10 +0530

    reset password page updated

--- base/client/src/app/components/advanced-inputs/input.util.ts
+++ base/client/src/app/components/advanced-inputs/input.util.ts
@@ -1,4 +1,4 @@
-import { FormGroup } from "@angular/forms";
+import { AbstractControl, FormGroup, ValidationErrors, ValidatorFn } from "@angular/forms";
 
 export function markFormGroupAsDirty(formGroup: FormGroup) {
     Object.values(formGroup.controls).forEach(control => {
@@ -6,13 +6,16 @@ export function markFormGroupAsDirty(formGroup: FormGroup) {
     });
 }
 
-export function passwordMatchValidator(formGroup: FormGroup) {
-  const password = formGroup.get('password');
-  const confirmPassword = formGroup.get('confirmPassword');
 
-  if (password && confirmPassword && (password?.value !== confirmPassword?.value)) {
-    confirmPassword?.setErrors({ mismatch: true });
+export const passwordMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirm_password');
+
+  if (password && confirmPassword && password.value !== confirmPassword.value) {
+    confirmPassword.setErrors({ mismatch: true });
+    return { mismatch: true }; 
   } else {
     confirmPassword?.setErrors(null);
   }
-}
+  return null;
+};
