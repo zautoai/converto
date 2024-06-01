a1c45cf3fc84732d8fecee6fc3097fb4c2d2a681
sridhar <149560572+sridharan1810@users.noreply.github.com>
2024-06-01 12:35:55 +0530

    component module

---
+++ base/client/src/app/components/advanced-inputs/advanced-input/advanced-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advanced-input',
+  templateUrl: './advanced-input.component.html',
+  styleUrl: './advanced-input.component.scss'
+})
+export class AdvancedInputComponent {
+
+}
